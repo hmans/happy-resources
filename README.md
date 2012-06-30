6804163092fcb256a5c3707967b429b99b987ab1
Hendrik Mans <hendrik@mans.de>
2012-06-30 15:37:16 +0200

    Update documentation.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 A collection of resource-focused controllers for the
 [Happy Web Application Toolkit](https://github.com/hmans/happy).
+
+* {Happy::Resources::ResourceController}, a generic Rails-style RESTful dispatcher.
+* {Happy::Resources::ActiveModelResourceController}, a complete RESTful resource controller serving ActiveModel compliant model classes.
--- lib/happy-resources/resource_controller.rb
+++ lib/happy-resources/resource_controller.rb
@@ -1,7 +1,41 @@
 module Happy
   module Resources
 
+    # ResourceController implements a generic RESTful controller in the style
+    # of RESTful Rails. Its #route method dispatches to the seven CRUD-like
+    # methods you're used from Rails depending on HTTP verb and URL:
+    #
+    #     Verb   URL         Method invoked
+    #     GET    /           #index
+    #     GET    /new        #new
+    #     POST   /           #create
+    #     GET    /:id        #show
+    #     GET    /:id/edit   #edit
+    #     PUT    /:id        #update
+    #     DELETE /:id        #destroy
+    #
+    # Simply derive a new controller class from this controller and implement
+    # the CRUD actions listed above:
+    #
+    #     class MyRestFulController < Happy::Resources::ResourceController
+    #       def index
+    #         @users = User.all
+    #         render 'index.erb'
+    #       end
+    #
+    #       def show
+    #         @user = User.find(params['id'])
+    #         render 'show.erb'
+    #       end
+    #     end
+    #
+    # For RESTfully serving an ActiveModel-compliant model class, you can use
+    # {Happy::Resources::ActiveModelResourceController ActiveModelResourceController}
+    # that derives from ResourceController and provides a common RESTful resource pattern.
+    #
     class ResourceController < Happy::Controller
+      # Dispatches the request to one of seven CRUD methods.
+      #
       def route
         on_get('new') { new }
 
